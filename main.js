9a41fb7bdd9e120343d6428b161ea3b2398377ed
Mikhail Bibochkin <extravaganych@gmail.com>
2020-10-09 21:55:29 +0300

    Урок №2 обязательный

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 60000; // доход в месяц
+let income = 'Фриланс'; // дополнительный доход
+let addExpenses = 'Сигареты, Бары, Таблетки'; // дополнительные расходы
+let deposit = true;
+let mission = 150000; // желаемые накопления
+let period = 9; // месяцев
 
-alert('Привет! Я - текст в модальном окне!');
+console.log('Типы данных: money - ', typeof(money), ', mission - ', typeof(mission), ', period - ', typeof(period));
+console.log('Длина строки addExpenses: ', addExpenses.length);
+console.log('Период равен ', period, ' месяцев');
+console.log('Цель: заработать ', mission, ' рублей');
 
-console.log('А я - в консоли!');
+addExpenses = addExpenses.toLowerCase();
+addExpenses = addExpenses.split(',');
+console.log(addExpenses);
+
+let budgetDay = money/30;
+console.log(budgetDay);
