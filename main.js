7ddde96c84ae4c098bbdaad79c0e80a109b50b5f
Mikhail Bibochkin <extravaganych@gmail.com>
2020-11-03 14:45:29 +0300

    Урок 15 обязательный часть 1

--- main.js
+++ main.js
@@ -30,23 +30,22 @@ let myForm = document.querySelector('body'),
 
 startButton.disabled = true;
 
-const AppData = function() {
-
-    
-    this.income = {};
-    this.addIncome = [];
-    this.expenses = {};
-    this.addExpenses = [];
-    this.deposit = false;
-    this.incomeMonth = 0;
-    this.percentDeposit = 0;
-    this.moneyDeposit = 0;
-    this.budget = 0;
-    this.budgetDay = 0;
-    this.budgetMonth = 0;
-    this.expensesMonth = 0;
-
-};
+class AppData {
+    constructor() {
+        this.income = {};
+        this.addIncome = [];
+        this.expenses = {};
+        this.addExpenses = [];
+        this.deposit = false;
+        this.incomeMonth = 0;
+        this.percentDeposit = 0;
+        this.moneyDeposit = 0;
+        this.budget = 0;
+        this.budgetDay = 0;
+        this.budgetMonth = 0;
+        this.expensesMonth = 0;
+    }
+}
 
 const appData = new AppData();
 
@@ -135,7 +134,7 @@ AppData.prototype.showResult = function() {
 };
 
 AppData.prototype.addExpensesBlock = function() {
-    let cloneExpensesItem = expensesItems[0].cloneNode(true);
+    const cloneExpensesItem = expensesItems[0].cloneNode(true);
     expensesItems[0].parentNode.insertBefore(cloneExpensesItem, expensesPlus);
     expensesItems = document.querySelectorAll('.expenses-items');
     if (expensesItems.length === 3) {
@@ -144,7 +143,7 @@ AppData.prototype.addExpensesBlock = function() {
 };
 
 AppData.prototype.addIncomeBlock = function() {
-    let cloneIncomeItem = incomeItems[0].cloneNode(true);
+    const cloneIncomeItem = incomeItems[0].cloneNode(true);
     incomeItems[0].parentNode.insertBefore(cloneIncomeItem, incomePlus);
     incomeItems = document.querySelectorAll('.income-items');
     if (incomeItems.length === 3) {
@@ -154,8 +153,8 @@ AppData.prototype.addIncomeBlock = function() {
 
 AppData.prototype.getExpenses = function() {
     const _this = this;
-    expensesItems.forEach(function(item){
-        let itemExpenses = item.querySelector('.expenses-title').value,
+    expensesItems.forEach((item) => {
+        const itemExpenses = item.querySelector('.expenses-title').value,
             cashExpenses = item.querySelector('.expenses-amount').value;
         if(itemExpenses !== '' && cashExpenses !== '') {
             _this.expenses[itemExpenses] = cashExpenses;
@@ -166,8 +165,8 @@ AppData.prototype.getExpenses = function() {
 
 AppData.prototype.getIncome = function() {
     const _this = this;
-    incomeItems.forEach(function(item){
-        let itemIncome = item.querySelector('.income-title').value,
+    incomeItems.forEach((item) => {
+        const itemIncome = item.querySelector('.income-title').value,
             cashIncome = item.querySelector('.income-amount').value;
         if(itemIncome !== '' && cashIncome !== '') {
             _this.income[itemIncome] = cashIncome;
@@ -179,7 +178,7 @@ AppData.prototype.getIncome = function() {
 AppData.prototype.getAddExpenses = function() {
     const _this = this;
     let addExpenses = additionalExpensesItem.value.split(',');
-    addExpenses.forEach(function(item){
+    addExpenses.forEach((item) => {
         item = item.trim();
         if(item !== '') {
             _this.addExpenses.push(item);
@@ -189,7 +188,7 @@ AppData.prototype.getAddExpenses = function() {
 
 AppData.prototype.getAddIncome = function() {
     const _this = this;
-    additionalIncomeItem.forEach(function(item){
+    additionalIncomeItem.forEach((item) => {
         let itemValue = item.value.trim();
         if (itemValue !== '') {
             _this.addIncome.push(itemValue);
@@ -216,14 +215,14 @@ AppData.prototype.calcSavedMoney = function() {
 
 AppData.prototype.eventListeners = function() {
     const _this = this;
-    startButton.addEventListener('click', function(){
+    startButton.addEventListener('click', () => {
         _this.start.apply(_this);
     });
     cancelButton.addEventListener('click', this.reset);
     expensesPlus.addEventListener('click', this.addExpensesBlock);
     incomePlus.addEventListener('click', this.addIncomeBlock);
     periodSelect.addEventListener('input', this.calcSavedMoney);
-    salaryAmount.addEventListener('input', function(){
+    salaryAmount.addEventListener('input', () => {
         if (salaryAmount.value === '') {
             startButton.disabled = true;
         } else {
